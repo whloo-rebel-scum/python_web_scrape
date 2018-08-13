ea26d4c148c3b34d29aa3e8837db8eb7ea7b91ce
Wesley <whloo.rebel.scum@outlook.com>
2018-08-13 12:43:47 -0700

    initial commit of unitrans_scrape

---
+++ unitrans_scrape.py
@@ -0,0 +1,31 @@
+import mechanicalsoup
+import requests
+
+from bs4 import BeautifulSoup
+
+
+# Connect to duckduckgo
+# browser = mechanicalsoup.StatefulBrowser()
+'''
+browser.open("https://duckduckgo.com/")
+browser.select_form('#search_form_homepage')
+browser["q"] = "MechanicalSoup"
+browser.submit_selected()
+
+for link in browser.get_current_page().select('a.result__a'):
+    print(link.text, '->', link.attrs['href'])
+'''
+# browser.open("https://unitrans.ucdavis.edu/routes/W/prediction")
+# browser.select_form('form[id="arrival-form"]')
+
+url = 'https://unitrans.ucdavis.edu/routes/W/prediction'
+
+with requests.Session() as session:
+    response = session.get(url)
+    soup = BeautifulSoup(response.content, "lxml")
+    print(soup.find_all(class_='time'))
+
+
+
+
+
