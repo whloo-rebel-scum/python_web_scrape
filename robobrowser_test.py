d86f67086b3381f045af138877d574769ad9fb71
Wesley <whloo.rebel.scum@outlook.com>
2018-08-14 12:56:18 -0700

    learning robobrowser

--- robobrowser_test.py
+++ robobrowser_test.py
@@ -1,4 +1,4 @@
-import re
+import re # regular expressions
 from robobrowser import RoboBrowser
 
 # Browse to Rap Genius
@@ -7,10 +7,11 @@ browser.open('http://rapgenius.com/')
 
 # Search for Queen
 form = browser.get_form(action='/search')
-form                # <RoboForm q=>
+form                # <RoboForm  q=>
 form['q'].value = 'queen'
 browser.submit_form(form)
 
+'''
 # Look up the first song
 songs = browser.select('.song_name')
 browser.follow_link(songs[0])
@@ -26,3 +27,4 @@ browser.follow_link('death on two legs')
 # Can also search HTML using regex patterns
 lyrics = browser.find(class_=re.compile(r'\blyrics\b'))
 lyrics.text         # \n[Verse 1]\nYou suck my blood like a leech...
+'''
