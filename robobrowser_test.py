8a2aae67140b078196743664ba0e635720ba648b
Wesley <whloo.rebel.scum@outlook.com>
2018-08-14 12:28:39 -0700

    added sample code from robobrowser website: http://robobrowser.readthedocs.io/en/latest/readme.html

---
+++ robobrowser_test.py
@@ -0,0 +1,28 @@
+import re
+from robobrowser import RoboBrowser
+
+# Browse to Rap Genius
+browser = RoboBrowser(history=True)
+browser.open('http://rapgenius.com/')
+
+# Search for Queen
+form = browser.get_form(action='/search')
+form                # <RoboForm q=>
+form['q'].value = 'queen'
+browser.submit_form(form)
+
+# Look up the first song
+songs = browser.select('.song_name')
+browser.follow_link(songs[0])
+lyrics = browser.select('.lyrics')
+lyrics[0].text      # \n[Intro]\nIs this the real life...
+
+# Back to results page
+browser.back()
+
+# Look up my favorite song
+browser.follow_link('death on two legs')
+
+# Can also search HTML using regex patterns
+lyrics = browser.find(class_=re.compile(r'\blyrics\b'))
+lyrics.text         # \n[Verse 1]\nYou suck my blood like a leech...
