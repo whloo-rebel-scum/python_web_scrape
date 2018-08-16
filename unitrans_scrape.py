faf016d9e7c530befd05b57af65fbc6424988350
Wesley <whloo.rebel.scum@outlook.com>
2018-08-16 13:05:23 -0700

    scraped all stops

--- unitrans_scrape.py
+++ unitrans_scrape.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select # for controlling dropdown boxes
+import pandas as pd
 
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
@@ -20,9 +22,19 @@ except TimeoutException:
     print("Timed out waiting for page to load")
     browser.quit()
 
-silo_arrival_times = browser.find_elements_by_xpath("//span[@class='time']")
-arrival_times = silo_arrival_times[0].text
-print('W LINE ARRIVAL TIMES (SILO): ', arrival_times, '\n')
+arrival_times = browser.find_elements_by_xpath("//span[@class='time']")
+a_t = arrival_times[0].text
+print('W LINE ARRIVAL TIMES', '\n')
+print('SILO AND CENTER ISLAND (E): ', a_t, '\n')
+
+# find the correct select tag
+select_stop = Select(browser.find_element_by_id('stop-select'))
+# select by visible text
+select_stop.select_by_visible_text('Hutchison & Art Building (E)')
+# select by value (Hutchison and Old Davis (E)
+select_stop.select_by_value('22000')
+# test print of all options
+[print(o.text) for o in select_stop.options]
 browser.close()
 
 
