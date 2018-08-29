7dd8351d6dccf9d77fb2c07e79c422e25cb50a00
Wesley <whloo.rebel.scum@outlook.com>
2018-08-29 12:23:38 -0700

    navigating csv file for user requests

--- unitrans_user_request.py
+++ unitrans_user_request.py
@@ -8,18 +8,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select  # for controlling drop-down boxes
 import pandas as pd
-import re
 import time
 
-start_time = time.clock()
-
 # read in csv file to var
+lines = pd.read_csv('bus_stop_options.csv')
 # print list of available lines, ask user to pick one (or quit (or show prediction times of saved stops)
-# print list of available stops from the chosen line, ask user to pick one
+print(lines['Route'].unique())  # isolate first col (Route)
+route_choice = input("Choose a bus line: ")
+print("Line ", route_choice, " chosen")
+# also option to delete a saved stop, or all
+
+# sort data frame according to user choice, only show stops for the chosen line
+# print list of relevant stops
+
 # retrieve prediction time
+start_time = time.clock()
+print("Retrieved prediction in --- %s seconds ---" % round(time.clock() - start_time, 2))
 # ask if user would like to save the stop (Y/N)
-# if yes, save to another csv file titled 'saved_stops'
+# if yes, save to another csv file titled 'saved_stops' (write to existing one, create new one if none)
 
 # loop until user chooses to end the program
 
-print("Scraping finished in --- %s seconds ---" % round(time.clock() - start_time, 2))
+
