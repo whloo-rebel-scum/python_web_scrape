103fac96638e41c6b3264d88b85067b993542580
Wesley <whloo.rebel.scum@outlook.com>
2018-09-07 11:23:52 -0700

    removed unneeded import

--- saved_stop_functions.py
+++ saved_stop_functions.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import csv
