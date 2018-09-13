ebe31f287680140357fd02e67c8aff7b3d648679
Wesley <whloo.rebel.scum@outlook.com>
2018-09-13 12:38:53 -0700

    added main

--- sports/hockey_team_stats.py
+++ sports/hockey_team_stats.py
@@ -8,6 +8,30 @@ from selenium.webdriver.support.ui import Select
 import pandas as pd
 import time
 
-# Goals:
-#   - scrape team stats for all 31 NHL teams
-#   - place them in panda dataframes for further analysis
+
+# - scrape team stats for all 31 NHL teams
+# - place them in panda dataframes for further analysis
+# - https://www.hockey-reference.com/teams/
+
+
+def main():
+    option = webdriver.ChromeOptions()
+    option.add_argument("--incognito")
+    browser = webdriver.Chrome(executable_path='C:\\Users\\whloo\\PycharmProjects\\chromedriver.exe',
+                               chrome_options=option)
+
+    teams_url = "https://www.hockey-reference.com/teams/"
+    browser.get(teams_url)
+    timeout = 20
+    try:
+        # Wait until the final element is loaded.
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@class='overthrow table_container']")))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+        browser.close()
+
+    browser.quit()
+
+
+main()
